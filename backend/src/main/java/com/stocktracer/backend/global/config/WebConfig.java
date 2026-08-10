e0ace90a5a83c735b2bc2687c9c51eb00cb166e4
최진현 <chlwlsgus543@gmail.com>
2026-08-10 10:29:40 +0900

    fix: config 주소 수정

--- backend/src/main/java/com/stocktracer/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/stocktracer/backend/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.stocktracer.backend.global;
+package com.stocktracer.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
