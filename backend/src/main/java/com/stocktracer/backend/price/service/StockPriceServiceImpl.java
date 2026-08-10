e5298965a696ebc65cd95e65fbb054dd5ebcf7df
최진현 <chlwlsgus543@gmail.com>
2026-08-10 16:02:59 +0900

    fix: StockPriceSaveBulkRequestDto 파일명 수정

--- backend/src/main/java/com/stocktracer/backend/price/service/StockPriceServiceImpl.java
+++ backend/src/main/java/com/stocktracer/backend/price/service/StockPriceServiceImpl.java
@@ -1,31 +1,25 @@
 package com.stocktracer.backend.price.service;
 
-import ch.qos.logback.core.joran.util.ConfigurationWatchListUtil;
 import com.stocktracer.backend.price.domain.StockPrice;
-import com.stocktracer.backend.price.dto.StockPriceBulkSaveRequestDto;
+import com.stocktracer.backend.price.dto.StockPriceSaveBulkRequestDto;
 import com.stocktracer.backend.price.dto.StockPriceResponseDto;
 import com.stocktracer.backend.price.dto.StockPriceSaveRequestDto;
 import com.stocktracer.backend.price.exception.DuplicateStockPriceException;
 import com.stocktracer.backend.price.exception.InvalidDateRangeException;
-import com.stocktracer.backend.price.exception.StockPriceBulkSaveException;
 import com.stocktracer.backend.price.exception.StockPriceNotFoundException;
 import com.stocktracer.backend.price.mapper.StockPriceMapper;
 import com.stocktracer.backend.price.service.interfaces.StockPriceService;
 import com.stocktracer.backend.stock.domain.StockInfo;
 import com.stocktracer.backend.stock.exception.StockInfoNotFoundException;
 import com.stocktracer.backend.stock.repository.interfaces.StockInfoRepository;
-import com.stocktracer.backend.stock.service.interfaces.StockInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.ListUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -77,7 +71,7 @@ public class StockPriceServiceImpl implements StockPriceService {
      */
     @Transactional
     @Override
-    public void bulkSave(StockPriceBulkSaveRequestDto bulkDto) {
+    public void bulkSave(StockPriceSaveBulkRequestDto bulkDto) {
 
         // 1. stockCode 분해
         List<String> stockCodes = bulkDto.prices().stream()
@@ -85,7 +79,12 @@ public class StockPriceServiceImpl implements StockPriceService {
                 .distinct()
                 .toList();
 
-        // 2. IN 쿼리로 StockInfo 일괄 조회 // 오라클의 경우 in 쿼리에서 1000건 이상 조회 시 에러 발생 하므로 배치 처리 후 조회
+        // 2. db 호출 전 중복 발견 시 거부 처리
+        validateNoDuplicationKeys(bulkDto.prices());
+
+        // 3. IN 쿼리로 StockInfo 일괄 조회 (jpa가 fk에 값을 넣을 시 객체를 통해 간접적으로 입력하므로 객체 조회 필요)
+        // in 쿼리로 한번에 조회하여 네트워크 낭비 방지
+        // db 부하를 막기 위해 배치 처리
         Map<String, StockInfo> stockInfoMap = new HashMap<>(stockCodes.size()); // 오버헤드 방지
         List<List<String>> codeBatches = ListUtils.partition(stockCodes, 1000);
         for(List<String> codeBatch : codeBatches){ // 바깥 루프: 배치 개수(N/1000)번 순회 // 안쪽: 배치 크기(최대 1000)만큼 순회
@@ -95,7 +94,7 @@ public class StockPriceServiceImpl implements StockPriceService {
         /** for문 내부에 foreach 효율이 괜찮은가? */
         // 다른 이중 FOR문과는 다르게 1000건 처리 후 다음 건(예: 1001번째)으로 넘어가기 때문에 중첩되지 않음 -> O(N)으로 해결
 
-        // 3. DTO -> StockPrice 도메인 객체 변환
+        // 4. DTO -> StockPrice 도메인 객체 변환
         List<StockPrice> prices = bulkDto.prices().stream() // 가독성 좋은 for문
                 .map(dto -> StockPrice.of(dto,findStockInfo(stockInfoMap, dto.stockCode()))) // 각 dto를 StockInfo와 조합해 객체로 변환
                 .toList();
@@ -103,9 +102,6 @@ public class StockPriceServiceImpl implements StockPriceService {
         /** collect와 map의 차이 */
         // map => 각 객체마다 수행해야 하는 로직이 필요할 시
         // collect => map 사용 여부와 관계없이 map이나 list를 최종 포장할 시 (list는 toList로 대체 사용 가능하나 map은 collect 필수)
-
-        // 4. 중복 발견 시 거부 처리
-        validateNoDuplicationKeys(prices);
 
         // 5. 1000개 단위로 분할하여 한 묶음씩 저장
         List<List<StockPrice>> batches = ListUtils.partition(prices, 1000); // 1000개 단위로 분할
@@ -120,13 +116,14 @@ public class StockPriceServiceImpl implements StockPriceService {
                 .orElseThrow(() -> new StockInfoNotFoundException(stockCode));
     }
 
-    // 중복 제거 로직
-    private void validateNoDuplicationKeys(List<StockPrice> prices){
+    // 중복 확인 로직
+    private void validateNoDuplicationKeys(List<StockPriceSaveRequestDto> prices){
         Set<String> seen = new HashSet<>();
-        for (StockPrice price : prices){
-            String key = price.getStockCode() + "_" + price.getStockDate(); // 복합키
+        for (StockPriceSaveRequestDto price : prices){
+            String key = price.stockCode() + "_" + price.stockDate(); // 복합키
             if(!seen.add(key)){
-                throw new DuplicateStockPriceException(price.getStockCode(), price.getStockDate());
+                // 추가가 불가하면 이미 존재하는 키
+                throw new DuplicateStockPriceException(price.stockCode(), price.stockDate());
             }
         }
     }
