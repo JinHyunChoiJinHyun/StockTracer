717b74bd525ca69355a1cb343c6a10a9f2142c4f
최진현 <chlwlsgus543@gmail.com>
2026-08-12 13:03:22 +0900

    fix: 주가 valid 수정

--- backend/src/main/java/com/stocktracer/backend/price/dto/StockPriceSaveRequestDto.java
+++ backend/src/main/java/com/stocktracer/backend/price/dto/StockPriceSaveRequestDto.java
@@ -4,6 +4,7 @@ import com.stocktracer.backend.price.domain.StockPrice;
 import com.stocktracer.backend.stock.domain.StockInfo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 
@@ -18,14 +19,14 @@ public record StockPriceSaveRequestDto(
         // domain에서 한번 더 검증 후 db 저장
     @NotBlank String stockCode,
     @NotNull LocalDate stockDate,
-    @NotNull @PositiveOrZero BigDecimal openPrice,
-    @NotNull @PositiveOrZero BigDecimal closePrice,
-    @NotNull @PositiveOrZero BigDecimal lowPrice,
-    @NotNull @PositiveOrZero BigDecimal highPrice,
+    @NotNull @Positive BigDecimal openPrice,
+    @NotNull @Positive BigDecimal closePrice,
+    @NotNull @Positive BigDecimal lowPrice,
+    @NotNull @Positive BigDecimal highPrice,
     BigDecimal priceChange,
-    @NotNull @PositiveOrZero Long volume,
-    @NotNull @PositiveOrZero BigDecimal tradingValue,
-    @NotNull @PositiveOrZero BigDecimal marketCap
+    @NotNull @PositiveOrZero Long volume, // 거래정지 시 0일 가능성 있음
+    @NotNull @PositiveOrZero BigDecimal tradingValue, // 거래정지 시 0일 가능성 있음
+    @NotNull @Positive BigDecimal marketCap
 ){
     public StockPrice toDomain(StockInfo stock){
         return StockPrice.of(this, stock);
